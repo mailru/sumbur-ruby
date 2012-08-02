e7ddc374e247417336a6b443da5a7ab0d4ff9ff8
Sokolov Yura 'funny-falcon <funny.falcon@gmail.com>
2012-08-02 20:44:50 +0400

    unroll first 23 loop steps

--- ext/sumbur/sumbur.c
+++ ext/sumbur/sumbur.c
@@ -18,18 +18,42 @@ rb_sumbur(VALUE self, VALUE hashed_int, VALUE capacity)
 
     n = 1;
 
-    for(i = 2; i <= capa; i++) {
-        c = L / (i * (i - 1));
-        if (c <= h) {
-            h -= c;
-        }
+    do {
+        if (h >= L / 2) h -= L / 2;
         else {
-            h += c * (i - n - 1);
-            n = i;
-            if (L / i - h < part) break;
+            n = 2;
+            if (L / 2 - h < part) break;
         }
-    }
+        if (capa == 2) break;
 
+#define curslice(i) (L / (i * (i - 1)))
+#define unroll(i) \
+        if (curslice(i) <= h) h -= curslice(i); \
+        else {                                  \
+            h += curslice(i) * (i - n - 1);     \
+            n = i;                              \
+            if (L / i - h < part) break;        \
+        }                                       \
+        if (capa == i) break
+
+        unroll(3); unroll(4); unroll(5); unroll(6); unroll(7);
+        unroll(8); unroll(9); unroll(10); unroll(11); unroll(12);
+        unroll(13); unroll(14); unroll(15); unroll(16); unroll(17);
+        unroll(18); unroll(19); unroll(20); unroll(21); unroll(22);
+        unroll(23); unroll(24);
+
+        for(i = 25; i <= capa; i++) {
+            c = L / (i * (i - 1));
+            if (c <= h) {
+                h -= c;
+            }
+            else {
+                h += c * (i - n - 1);
+                n = i;
+                if (L / i - h < part) break;
+            }
+        }
+    } while(0);
     return INT2FIX(n - 1);
 }
 
